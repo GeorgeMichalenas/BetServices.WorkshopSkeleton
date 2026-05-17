61e16fd04694ce1040dc7ccd316b530ccc3344e3
gmichalenas <gmichalenas>
2026-05-17 15:26:03 +0300

    betService workshop code

---
+++ BetService.WorkshopSkeleton.Api/Controllers/BetsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BetService.WorkshopSkeleton.Api.Controllers
+{
+	[ApiController]
+	[Route("bets")]
+	public class BetsController : ControllerBase
+	{
+	}
+}
---
+++ BetService.WorkshopSkeleton.Infrastructure/Repositories/InMemoryBetRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace BetService.WorkshopSkeleton.Infrastructure.Repositories
+{
+	public class InMemoryBetRepository
+	{
+		private readonly IMemoryCache _memoryCache;
+
+		public InMemoryBetRepository(IMemoryCache memoryCache)
+		{
+			_memoryCache = memoryCache;
+		}
+
+		public async Task AddAsync(Bet bet, CancellationToken cancellationToken)
+		{
+			_memoryCache.Set(bet.Id, bet);
+			await Task.CompletedTask;
+		}
+
+		public async Task<Bet?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+		{
+			_memoryCache.TryGetValue(id, out Bet? bet);
+			return await Task.FromResult(bet);
+		}
+	}
+}
